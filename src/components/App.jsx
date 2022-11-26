e460e9e4affa1e0caa79f2c0c5dde4eade5f86e3
OlenaBielova <belova.elena11@gmail.com>
2022-11-26 18:17:22 +0200

    add form and markup render

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,41 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import { nanoid } from 'nanoid';
+import { Form } from './Form';
+
+export class App extends React.Component {
+  state = {
+    contacts: [],
+  };
+
+  formSubmitHandler = data => {
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, data],
+    }));
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          // display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <h3>Phonebook</h3>
+        <Form onSubmit={this.formSubmitHandler}></Form>
+        <h3>Contacts</h3>
+        <ul>
+          {this.state.contacts.map(contact => (
+            <li key={nanoid()}>
+              {contact.name}: {contact.number}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
